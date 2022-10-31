abc81624aca0a4700b07732bacfeab2c9dc8f2ea
yorik100 <yorik100@hotmail.com>
2022-10-31 15:19:05 +0100

    Added supported champ list

--- OpenSeries/dllmain.cpp
+++ OpenSeries/dllmain.cpp
@@ -9,9 +9,19 @@ PLUGIN_API bool on_sdk_load( plugin_sdk_core* plugin_sdk_good )
 {
     DECLARE_GLOBALS( plugin_sdk_good );
 
-    brand::load();
-
-    return true;
+    switch (myhero->get_champion())
+    {
+    case champion_id::Brand:
+        // Load Brand script
+        //
+        brand::load();
+        break;
+    default:
+        // We don't support this champ, print message and return false (core will not load this plugin and on_sdk_unload will be never called)
+        //
+        console->print("Champion %s is not supported!", myhero->get_model_cstr());
+        return false;
+    }
 }
 
 PLUGIN_API void on_sdk_unload( )
