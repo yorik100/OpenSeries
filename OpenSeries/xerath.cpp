623513454dd1c264c3d0f3d36716b2ea7d499449
yorik100 <yorik100@hotmail.com>
2022-11-15 19:31:57 +0100

    Pred values fix

--- OpenSeries/xerath.cpp
+++ OpenSeries/xerath.cpp
@@ -1984,7 +1984,7 @@ namespace xerath {
 
 		// R
 		r = plugin_sdk->register_spell(spellslot::r, XERATH_R_RANGE);
-		r->set_skillshot(0.6f, 200.f, FLT_MAX, { collisionable_objects::yasuo_wall }, skillshot_type::skillshot_circle);
+		r->set_skillshot(0.6f, 200.f, FLT_MAX, {}, skillshot_type::skillshot_circle);
 		r->set_spell_lock(false);
 
 		// Q dummy
