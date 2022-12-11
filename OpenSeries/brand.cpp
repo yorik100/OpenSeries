ebddf91efd2797f53a8ecaf5cc9df97a820ac715
yorik100 <yorik100@hotmail.com>
2022-12-11 23:42:19 +0100

    Better invul checks 2

--- OpenSeries/brand.cpp
+++ OpenSeries/brand.cpp
@@ -808,8 +808,8 @@ namespace brand {
 		// If it's Yuumi that is attached then target is not valid
 		if (isYuumiAttached(target)) return false;
 
-		// Extra invulnerability checks
-		if ((int)target->get_action_state() == 167772162 && !target->is_targetable() && stunTime[target->get_handle()] < 0 && !target->get_active_spell()) return false;
+		// Extra invulnerability checks (hextech portal)
+		if ((int)target->get_action_state() == 167772162 && !target->is_targetable() && target->is_dashing() && !target->get_active_spell()) return false;
 
 		const auto& isCastingImmortalitySpell = target->get_active_spell() && immuneSpells.contains(target->get_active_spell()->get_spell_data()->get_name_hash());
 		const auto& isValid = !isCastingImmortalitySpell && (target->is_valid_target(range, from, invul) || isValidRecalling(target, range, from));
