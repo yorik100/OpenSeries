281ffd51bac4f2a38b2f069bc7fd3bd157a12f64
yorik100 <yorik100@hotmail.com>
2022-12-11 19:26:23 +0100

    Better!

--- OpenSeries/xerath.cpp
+++ OpenSeries/xerath.cpp
@@ -1022,7 +1022,7 @@ namespace xerath {
 		if (isYuumiAttached(target)) return false;
 
 		// Extra invulnerability checks
-		if (target->get_action_state() == (action_state)167772162) return false;
+		if ((int)target->get_action_state() == 167772162 && !target->is_targetable()) return false;
 
 		const auto& isCastingImmortalitySpell = target->get_active_spell() && immuneSpells.contains(target->get_active_spell()->get_spell_data()->get_name_hash());
 		const auto& isValid = !isCastingImmortalitySpell && (target->is_valid_target(range, from, invul) || isValidRecalling(target, range, from));
