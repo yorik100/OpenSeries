e47745e6832de260169b795949925a8073a53b7e
yorik100 <yorik100@hotmail.com>
2023-05-05 10:42:31 +0200

    Not needed

--- OpenSeries/brand.cpp
+++ OpenSeries/brand.cpp
@@ -601,9 +601,8 @@ namespace brand {
 	prediction_output getQPred(const game_object_script& target)
 	{
 		// Get Q pred
-		const auto& totalRadius = std::max(target->get_bounding_radius(), 65.f);
-		q->set_range(BRAND_Q_RANGE + std::max(target->get_bounding_radius(), 65.f));
-		q->from = myhero->get_position().distance(target->get_position()) > totalRadius ? myhero->get_position().extend(target->get_position(), totalRadius) : target->get_position();
+		
+		q->set_range(BRAND_Q_RANGE + std::max(target->get_bounding_radius(), 65.f);
 		prediction_output p = q->get_prediction(target);
 		if (p.hitchance <= static_cast<hit_chance>(2)) return p;
 
